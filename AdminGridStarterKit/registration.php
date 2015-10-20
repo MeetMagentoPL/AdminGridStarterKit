4e049a57fb88d283f8e522c1b8b17d828fca2961
Nikola Lazic <nlazic@gorillagroup.com>
2015-10-20 15:54:04 +0200

    Initialization files

---
+++ AdminGridStarterKit/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This is an activation file present in the develop branch only
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MeetMagentoPL_AdminGridStarterKit',
+    __DIR__
+);
